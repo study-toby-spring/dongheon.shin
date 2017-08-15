7223513cd844cd2486d32bfe725415f9b14bf749
dongheon.shin <leucosian@gmail.com>
2017-08-15 16:26:36 +0900

    SLF4J / Log4J 설정

--- src/main/java/com/webtoonscorp/spring/controller/HelloController.java
+++ src/main/java/com/webtoonscorp/spring/controller/HelloController.java
@@ -1,15 +1,22 @@
 package com.webtoonscorp.spring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping(method = { RequestMethod.GET, RequestMethod.POST }, path = "/hello")
-    public ModelAndView hello() {
+    public ModelAndView hello(@RequestParam("hi") String message) {
+
+        logger.info("Hi : {}", message);
 
         ModelAndView mav = new ModelAndView();
 
