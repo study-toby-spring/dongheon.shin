180363a7e24659df79fa60fc848639058d045b1f
dongheon.shin <leucosian@gmail.com>
2017-08-16 22:43:23 +0900

    [3.3] JDBC 전략 패턴의 최적화
    - 로컬 클래스를 익명 클래스로 전환

--- src/main/java/com/webtoonscorp/spring/repository/UserDao.java
+++ src/main/java/com/webtoonscorp/spring/repository/UserDao.java
@@ -23,7 +23,7 @@ public class UserDao {
 
     public void add(final User user) throws SQLException {
 
-        class AddStatement implements Statement {
+        jdbcContextWithStatement(new Statement() {
 
             public PreparedStatement getPreparedStatement(Connection connection) throws SQLException {
 
@@ -35,10 +35,7 @@ public class UserDao {
 
                 return preparedStatement;
             }
-        }
-
-        Statement statement = new AddStatement();
-        jdbcContextWithStatement(statement);
+        });
     }
 
     public User get(String id) throws SQLException {
@@ -115,16 +112,13 @@ public class UserDao {
 
     public void deleteAll() throws SQLException {
 
-        class DeleteAllStatement implements Statement {
+        jdbcContextWithStatement(new Statement() {
 
             public PreparedStatement getPreparedStatement(Connection connection) throws SQLException {
 
                 return connection.prepareStatement("delete * from users");
             }
-        }
-
-        Statement statement = new DeleteAllStatement();
-        jdbcContextWithStatement(statement);
+        });
     }
 
     private void jdbcContextWithStatement(Statement statement) throws SQLException {
