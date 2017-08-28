834fc6c627dce3b3ec0abd7edcae79a7f77bff77
dongheon.shin <leucosian@gmail.com>
2017-08-28 23:19:29 +0900

    [5.4] 메일 서비스 추상화
    - 표준 MailSender를 사용하여 서비스 기능 추상화

--- src/main/java/com/webtoonscorp/spring/service/UserService.java
+++ src/main/java/com/webtoonscorp/spring/service/UserService.java
@@ -3,29 +3,26 @@ package com.webtoonscorp.spring.service;
 import com.webtoonscorp.spring.domain.User;
 import com.webtoonscorp.spring.repository.UserDao;
 import com.webtoonscorp.spring.type.Level;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.util.List;
-import java.util.Properties;
 
 public class UserService {
 
     public static final int MIN_LOG_COUNT_FOR_SILVER = 50;
     public static final int MIN_RECOMMEND_FOR_GOLD = 30;
 
-    private Authenticator authenticator = new javax.mail.Authenticator() {
-        protected PasswordAuthentication getPasswordAuthentication() {
-            return new PasswordAuthentication("<아이디>", "<암호>");
-        }
-    };
-
+    private MailSender mailSender;
     private UserDao userDao;
     private PlatformTransactionManager manager;
+
+    public void setMailSender(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
@@ -75,40 +72,14 @@ public class UserService {
 
     private void sendUpgradeMail(User user) {
 
-        Properties properties = new Properties();
+        SimpleMailMessage message = new SimpleMailMessage();
 
-        // TODO : 보안 설정
-        //
-        // 1. authenticator 계정, 암호 입력
-        //
-        // - 커밋 시 실제 암호가 유출되지 않도록 유의할 것
-        //
-        // 2. 보안 설정 변경
-        //
-        // - Console 에 출력되는 링크로 접속해서 '보안 수준이 낮은 앱에 대한 계정 액세스 변경' 선택 후 'Allow less secure apps' 항목 ON 으로 변경
-        // - TC 수행 후, 제목이 'Review blocked sign-in attempt'인 메일을 수신하면 내용을 열고 허용
-        //
+        message.setTo(user.getEmail());
+        message.setFrom("admin@ksug.org");
+        message.setSubject("Upgrade 안내");
+        message.setText("사용자님의 등급이 " + user.getLevel().name() + " 로 업그레이드 되었습니다.");
 
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(properties, authenticator);
-        MimeMessage message = new MimeMessage(session);
-
-        try {
-
-            message.setFrom(new InternetAddress("admin@ksug.org"));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
-            message.setSubject("Upgrade 안내");
-            message.setText("사용자님의 등급이 " + user.getLevel().name() + " 로 업그레이드 되었습니다.");
-
-            Transport.send(message);
-
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+        mailSender.send(message);
     }
 
     private boolean canUpgradeLevel(User user) {
