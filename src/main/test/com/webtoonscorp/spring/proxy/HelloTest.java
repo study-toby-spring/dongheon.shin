0edbc23d1a7706e0d247446543ff992cef10bd09
dongheon.shin <leucosian@gmail.com>
2017-09-10 23:08:17 +0900

    [6.5] 스프링의 AOP
    - 확장된 포인트 컷 테스트

--- src/main/test/com/webtoonscorp/spring/proxy/HelloTest.java
+++ src/main/test/com/webtoonscorp/spring/proxy/HelloTest.java
@@ -1,6 +1,8 @@
 package com.webtoonscorp.spring.proxy;
 
 import org.junit.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
@@ -77,5 +79,57 @@ public class HelloTest {
         assertThat(proxy.sayHello("dongheon"), is("HELLO, DONGHEON"));
         assertThat(proxy.sayHi("dongheon"), is("HI, DONGHEON"));
         assertThat(proxy.sayThankYou("dongheon"), is("Thank you, dongheon"));
+    }
+
+    @Test
+    public void classNamePointcutAdvisor() {
+
+        NameMatchMethodPointcut classMethodPointcut = new NameMatchMethodPointcut() {
+
+            @Override
+            public ClassFilter getClassFilter() {
+
+                return new ClassFilter() {
+
+                    public boolean matches(Class<?> type) {
+
+                        String name = type.getSimpleName();
+                        return name.startsWith("HelloT") || name.startsWith("HelloD");
+                    }
+                };
+            }
+        };
+
+        classMethodPointcut.setMappedName("sayH*");
+
+        class HelloWorld extends HelloTarget { }
+        class HelloDongheon extends HelloTarget { }
+
+        checkAdvised(new HelloTarget(), classMethodPointcut, true);
+        checkAdvised(new HelloWorld(), classMethodPointcut, false);
+        checkAdvised(new HelloDongheon(), classMethodPointcut, true);
+    }
+
+    private void checkAdvised(Object target, Pointcut pointcut, boolean advised) {
+
+        ProxyFactoryBean bean = new ProxyFactoryBean();
+
+        bean.setTarget(target);
+        bean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new HelloAdvice()));
+
+        Hello proxy = (Hello) bean.getObject();
+
+        if (advised) {
+
+            assertThat(proxy.sayHello("dongheon"), is("HELLO, DONGHEON"));
+            assertThat(proxy.sayHi("dongheon"), is("HI, DONGHEON"));
+            assertThat(proxy.sayThankYou("dongheon"), is("Thank you, dongheon"));
+        }
+        else {
+
+            assertThat(proxy.sayHello("dongheon"), is("Hello, dongheon"));
+            assertThat(proxy.sayHi("dongheon"), is("Hi, dongheon"));
+            assertThat(proxy.sayThankYou("dongheon"), is("Thank you, dongheon"));
+        }
     }
 }
