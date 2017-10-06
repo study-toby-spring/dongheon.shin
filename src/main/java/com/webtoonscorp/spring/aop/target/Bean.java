9b9a792eb572bfa31da612cb5310c5a185e9d343
leucosian <leucosian@gmail.com>
2017-10-06 19:46:48 +0900

    [6.5] Spring AOP
    - AspectJ 표현식 테스트 확장

---
+++ src/main/java/com/webtoonscorp/spring/aop/target/Bean.java
@@ -0,0 +1,8 @@
+package com.webtoonscorp.spring.aop.target;
+
+public class Bean {
+
+    public void method() throws RuntimeException {
+
+    }
+}
--- src/main/test/com/webtoonscorp/spring/aop/PointcutTest.java
+++ src/main/test/com/webtoonscorp/spring/aop/PointcutTest.java
@@ -1,5 +1,6 @@
 package com.webtoonscorp.spring.aop;
 
+import com.webtoonscorp.spring.aop.target.Bean;
 import com.webtoonscorp.spring.aop.target.Target;
 import org.junit.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
@@ -17,5 +18,29 @@ public class PointcutTest {
 
         assertThat(pointcut.getClassFilter().matches(Target.class) && pointcut.getMethodMatcher().matches(Target.class.getMethod("plus", int.class, int.class), null), is(false));
         assertThat(pointcut.getClassFilter().matches(Target.class) && pointcut.getMethodMatcher().matches(Target.class.getMethod("minus", int.class, int.class), null), is(true));
+    }
+
+    @Test
+    public void pointcut() throws Exception {
+
+        targetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+    }
+
+    public void targetClassPointcutMatches(String expression, Boolean... expected) throws Exception {
+
+        pointcutMatches(expression, expected[0], Target.class, "hello");
+        pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+        pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+        pointcutMatches(expression, expected[3], Target.class, "minus", int.class, int.class);
+        pointcutMatches(expression, expected[4], Target.class, "method");
+        pointcutMatches(expression, expected[5], Bean.class, "method");
+    }
+
+    public void pointcutMatches(String expression, boolean expected, Class<?> type, String method, Class<?>... args) throws Exception {
+
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+
+        pointcut.setExpression(expression);
+        assertThat(pointcut.getClassFilter().matches(type) && pointcut.getMethodMatcher().matches(type.getMethod(method, args), null), is(expected));
     }
 }
