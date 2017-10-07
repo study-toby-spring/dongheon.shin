737263a7abfe45df92249c4110608f21a0af3d13
leucosian <leucosian@gmail.com>
2017-10-07 13:46:22 +0900

    [6.7] 어노테이션 트랜잭션 속성과 포인트 컷
    - @Transactional 어노테이션을 활용하여 트랜잭션 속성 부여
    - Tx 네임스페이스를 활용하여 설정 간소화

--- src/main/java/com/webtoonscorp/spring/service/UserService.java
+++ src/main/java/com/webtoonscorp/spring/service/UserService.java
@@ -1,17 +1,21 @@
 package com.webtoonscorp.spring.service;
 
 import com.webtoonscorp.spring.domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
 
     void add(User user);
-
-    User get(String id);
-    List<User> getAll();
     void deleteAll();
     void update(User user);
-
     void upgradeLevels();
+
+    @Transactional(readOnly = true)
+    User get(String id);
+
+    @Transactional(readOnly = true)
+    List<User> getAll();
 }
